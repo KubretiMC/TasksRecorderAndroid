a6594a4dedbba094fad6baa6ef4167c165b76c0b
ivanovDragia <ivanovdragia1@gmail.com>
2020-02-26 10:53:08 +0200

    Commit 2

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button ShowPendingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ShowPendingButton = findViewById(R.id.ShowPending);
+        ShowPendingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenShowPendingActivity();
+            }
+        });
+    }
+
+    public void OpenShowPendingActivity() {
+        Intent intent = new Intent(this, ShowPendingActivity.class);
+        startActivity(intent);
     }
 }
